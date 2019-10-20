0ddec97a093567aec4c1e26b5a5a902a18b0cdce
guilhermeparente17 <guilherme.parente17@outlook.com>
2019-10-19 23:03:37 -0300

    Mudando o estado do Component App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      string: 'Hello Guilherme'
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Guilherme</h2>
+          <h2>{this.state.string}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => this.setState({ string: "Deu certo"})}>Change Text</button>
       </div>
     );
   }
