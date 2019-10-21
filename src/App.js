18a8e389e5bb61eff77b33457ecbd2385159f675
guilhermeparente17 <guilherme.parente17@outlook.com>
2019-10-20 21:10:13 -0300

    Adicionando estilos, card-component e ajustando o app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
 
 class App extends Component {
 
@@ -7,9 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-   
-    ]
+      monsters: []
     }
   }
 
@@ -25,9 +24,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => <h1 key> {monster.id} {monster.name}</h1>)
-        }
+        <CardList monsters={this.state.monsters}>
+          
+        </CardList>
+        
       </div>
     );
   }
